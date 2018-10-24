341d6283de4a38cd93f301c5a891324139a2ebef
Marko <marko.ristin@gmail.com>
2018-10-24 11:45:36 +0200

    benchmarked startup time of modules with contracts

---
+++ benchmarks/startup/classes_100_with_no_invariant.py
@@ -0,0 +1,802 @@
+#!/usr/bin/env python3
+import icontract
+
+
+class SomeClass0:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass1:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass2:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass3:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass4:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass5:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass6:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass7:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass8:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass9:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass10:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass11:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass12:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass13:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass14:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass15:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass16:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass17:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass18:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass19:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass20:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass21:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass22:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass23:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass24:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass25:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass26:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass27:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass28:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass29:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass30:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass31:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass32:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass33:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass34:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass35:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass36:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass37:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass38:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass39:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass40:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass41:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass42:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass43:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass44:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass45:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass46:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass47:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass48:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass49:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass50:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass51:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass52:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass53:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass54:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass55:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass56:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass57:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass58:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass59:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass60:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass61:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass62:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass63:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass64:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass65:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass66:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass67:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass68:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass69:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass70:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass71:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass72:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass73:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass74:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass75:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass76:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass77:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass78:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass79:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass80:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass81:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass82:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass83:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass84:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass85:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass86:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass87:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass88:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass89:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass90:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass91:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass92:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass93:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass94:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass95:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass96:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass97:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass98:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
+
+
+class SomeClass99:
+    def __init__(self) -> None:
+        self.x = 100
+
+    def some_func(self) -> None:
+        pass
---
+++ benchmarks/startup/generate.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+"""Generate a module source code containing functions to benchmark the start-up time of the library with contracts."""
+
+import argparse
+import io
+import os
+import pathlib
+import sys
+import textwrap
+
+
+def generate_functions(functions: int, contracts: int, disabled: bool) -> str:
+    out = io.StringIO()
+    out.write("#!/usr/bin/env python3\n")
+    out.write("import icontract\n\n")
+
+    for i in range(0, functions):
+        if i > 0:
+            out.write("\n")
+
+        for j in range(0, contracts):
+            if not disabled:
+                out.write("@icontract.require(lambda x: x > {})\n".format(j))
+            else:
+                out.write("@icontract.require(lambda x: x > {}, enabled=False)\n".format(j))
+
+        out.write("def some_func{}(x: int) -> None:\n    pass\n".format(i))
+
+    return out.getvalue()
+
+
+def generate_classes(classes: int, invariants: int, disabled: bool) -> str:
+    out = io.StringIO()
+    out.write("#!/usr/bin/env python3\n")
+    out.write("import icontract\n\n")
+
+    for i in range(0, classes):
+        if i > 0:
+            out.write("\n")
+
+        for j in range(0, invariants):
+            if not disabled:
+                out.write("@icontract.invariant(lambda self: self.x > {})\n".format(j))
+            else:
+                out.write("@icontract.invariant(lambda self: self.x > {}, enabled=False)\n".format(j))
+
+        out.write(
+            textwrap.dedent("""\
+            class SomeClass{}:
+                def __init__(self) -> None:
+                    self.x = 100
+                    
+                def some_func(self) -> None:
+                    pass
+            """.format(i)))
+
+    return out.getvalue()
+
+
+def main() -> None:
+    """"Execute the main routine."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--outdir", help="output directory", default=os.path.dirname(__file__))
+    args = parser.parse_args()
+
+    outdir = pathlib.Path(args.outdir)
+    if not outdir.exists():
+        raise FileNotFoundError("Output directory is missing: {}".format(outdir))
+
+    for contracts in [0, 1, 5, 10]:
+        if contracts == 0:
+            pth = outdir / "functions_100_with_no_contract.py"
+        elif contracts == 1:
+            pth = outdir / "functions_100_with_1_contract.py"
+        else:
+            pth = outdir / "functions_100_with_{}_contracts.py".format(contracts)
+
+        text = generate_functions(functions=100, contracts=contracts, disabled=False)
+        pth.write_text(text)
+
+    for contracts in [1, 5, 10]:
+        if contracts == 1:
+            pth = outdir / "functions_100_with_1_disabled_contract.py"
+        else:
+            pth = outdir / "functions_100_with_{}_disabled_contracts.py".format(contracts)
+
+        text = generate_functions(functions=100, contracts=contracts, disabled=True)
+        pth.write_text(text)
+
+    for invariants in [0, 1, 5, 10]:
+        if invariants == 0:
+            pth = outdir / "classes_100_with_no_invariant.py"
+        elif invariants == 1:
+            pth = outdir / "classes_100_with_1_invariant.py"
+        else:
+            pth = outdir / "classes_100_with_{}_invariants.py".format(invariants)
+
+        text = generate_classes(classes=100, invariants=invariants, disabled=False)
+        pth.write_text(text)
+
+    for invariants in [1, 5, 10]:
+        if invariants == 1:
+            pth = outdir / "classes_100_with_1_disabled_invariant.py"
+        else:
+            pth = outdir / "classes_100_with_{}_disabled_invariants.py".format(invariants)
+
+        text = generate_classes(classes=100, invariants=invariants, disabled=True)
+        pth.write_text(text)
+
+
+if __name__ == "__main__":
+    main()
