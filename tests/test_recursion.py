043a48e985ec1c83d7100a56ab371a9aee4c7602
Marko Ristin <marko.ristin@gmail.com>
2020-04-24 21:20:23 +0200

    added additional test cases for recursion in the contracts

--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -6,8 +6,8 @@ from typing import List
 import icontract
 
 
-class TestOK(unittest.TestCase):
-    def test_recursive_preconditions(self) -> None:
+class TestPrecondition(unittest.TestCase):
+    def test_ok(self) -> None:
         order = []  # type: List[str]
 
         @icontract.require(lambda: another_func())  # pylint: disable=unnecessary-lambda
@@ -35,7 +35,100 @@ class TestOK(unittest.TestCase):
         self.assertListEqual(['yet_another_func', 'yet_yet_another_func', 'some_func', 'another_func', 'some_func'],
                              order)
 
-    def test_recursive_postconditions(self) -> None:
+    def test_recover_after_exception(self) -> None:
+        order = []  # type: List[str]
+        some_func_should_raise = True
+
+        class CustomError(Exception):
+            pass
+
+        @icontract.require(lambda: another_func())  # pylint: disable=unnecessary-lambda
+        @icontract.require(lambda: yet_another_func())  # pylint: disable=unnecessary-lambda
+        def some_func() -> bool:
+            order.append(some_func.__name__)
+            if some_func_should_raise:
+                raise CustomError('some_func_should_raise')
+            return True
+
+        @icontract.require(lambda: some_func())  # pylint: disable=unnecessary-lambda
+        @icontract.require(lambda: yet_yet_another_func())  # pylint: disable=unnecessary-lambda
+        def another_func() -> bool:
+            order.append(another_func.__name__)
+            return True
+
+        def yet_another_func() -> bool:
+            order.append(yet_another_func.__name__)
+            return True
+
+        def yet_yet_another_func() -> bool:
+            order.append(yet_yet_another_func.__name__)
+            return True
+
+        try:
+            some_func()
+        except CustomError:
+            pass
+
+        self.assertListEqual(['yet_another_func', 'yet_yet_another_func', 'some_func'], order)
+
+        # Reset for the next experiment
+        order = []
+        some_func_should_raise = False
+
+        some_func()
+
+        self.assertListEqual(['yet_another_func', 'yet_yet_another_func', 'some_func', 'another_func', 'some_func'],
+                             order)
+
+
+class TestPostcondition(unittest.TestCase):
+    def test_ok(self) -> None:
+        order = []  # type: List[str]
+        another_func_should_raise = True
+
+        class CustomError(Exception):
+            pass
+
+        @icontract.ensure(lambda: another_func())  # pylint: disable=unnecessary-lambda
+        @icontract.ensure(lambda: yet_another_func())  # pylint: disable=unnecessary-lambda
+        def some_func() -> bool:
+            order.append(some_func.__name__)
+            return True
+
+        @icontract.ensure(lambda: some_func())  # pylint: disable=unnecessary-lambda
+        @icontract.ensure(lambda: yet_yet_another_func())  # pylint: disable=unnecessary-lambda
+        def another_func() -> bool:
+            order.append(another_func.__name__)
+            if another_func_should_raise:
+                raise CustomError('some_func_should_raise')
+
+            return True
+
+        def yet_another_func() -> bool:
+            order.append(yet_another_func.__name__)
+            return True
+
+        def yet_yet_another_func() -> bool:
+            order.append(yet_yet_another_func.__name__)
+            return True
+
+        try:
+            some_func()
+        except CustomError:
+            pass
+
+        self.assertListEqual(['some_func', 'yet_another_func', 'another_func'], order)
+
+        # Reset for the next experiments
+        order = []
+        another_func_should_raise = False
+
+        some_func()
+
+        self.assertListEqual(['some_func', 'yet_another_func', 'another_func', 'yet_yet_another_func', 'some_func'],
+                             order)
+
+    def test_recover_after_exception(self) -> None:
         order = []  # type: List[str]
 
         @icontract.ensure(lambda: another_func())  # pylint: disable=unnecessary-lambda
@@ -63,7 +156,9 @@ class TestOK(unittest.TestCase):
         self.assertListEqual(['some_func', 'yet_another_func', 'another_func', 'yet_yet_another_func', 'some_func'],
                              order)
 
-    def test_recursive_invariants(self) -> None:
+
+class TestInvariant(unittest.TestCase):
+    def test_ok(self) -> None:
         order = []  # type: List[str]
 
         @icontract.invariant(lambda self: self.some_func())  # pylint: disable=no-member
@@ -84,6 +179,50 @@ class TestOK(unittest.TestCase):
 
         # Reset for the next experiment
         order = []
+
+        some_instance.another_func()
+        self.assertListEqual(['some_func', 'another_func', 'some_func'], order)
+
+    def test_recover_after_exception(self) -> None:
+        order = []  # type: List[str]
+        some_func_should_raise = False
+
+        class CustomError(Exception):
+            pass
+
+        @icontract.invariant(lambda self: self.some_func())  # pylint: disable=no-member
+        class SomeClass(icontract.DBC):
+            def __init__(self) -> None:
+                order.append('__init__')
+
+            def some_func(self) -> bool:
+                order.append('some_func')
+                if some_func_should_raise:
+                    raise CustomError('some_func_should_raise')
+
+                return True
+
+            def another_func(self) -> bool:
+                order.append('another_func')
+                return True
+
+        some_instance = SomeClass()
+        self.assertListEqual(['__init__', 'some_func'], order)
+
+        # Reset for the next experiment
+        order = []
+        some_func_should_raise = True
+
+        try:
+            some_instance.another_func()
+        except CustomError:
+            pass
+
+        self.assertListEqual(['some_func'], order)
+
+        # Reset for the next experiment
+        order = []
+        some_func_should_raise = False
 
         some_instance.another_func()
         self.assertListEqual(['some_func', 'another_func', 'some_func'], order)
