cc3c0860c050a22a66e5c73b2b0e062a2487ab20
Marko Ristin <marko.ristin@gmail.com>
2020-10-04 16:10:01 +0200

    Formatted benchmark scripts
    Benchmark scripts were not included in the precommit.py. This change
    merely reformats them, while their inclusion in precommit.py will
    happen in a future pull request.

--- benchmarks/against_dpcontracts/compare_invariant.py
+++ benchmarks/against_dpcontracts/compare_invariant.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 Benchmark icontract against dpcontracts and no contracts.
 
@@ -14,6 +13,7 @@ import dpcontracts
 import tabulate
 
 import icontract
+
 
 @icontract.invariant(lambda self: len(self.parts) > 0)
 class ClassWithIcontract:
@@ -53,7 +53,8 @@ clses = [
     'ClassWithInlineContract',
 ]
 
-def writeln_utf8(text: str)->None:
+
+def writeln_utf8(text: str) -> None:
     """
     write the text to STDOUT using UTF-8 encoding followed by a new-line character.
 
@@ -63,16 +64,14 @@ def writeln_utf8(text: str)->None:
     sys.stdout.buffer.write(text.encode('utf-8'))
     sys.stdout.buffer.write(os.linesep.encode('utf-8'))
 
+
 def measure_invariant_at_init() -> None:
     durations = [0.0] * len(clses)
 
     number = 1 * 1000 * 1000
 
     for i, cls in enumerate(clses):
-        duration = timeit.timeit(
-            "{}('X.Y')".format(cls),
-            setup="from __main__ import {}".format(cls),
-            number=number)
+        duration = timeit.timeit("{}('X.Y')".format(cls), setup="from __main__ import {}".format(cls), number=number)
         durations[i] = duration
 
     writeln_utf8("Benchmarking invariant at __init__:\n")
@@ -106,9 +105,7 @@ def measure_invariant_at_function() -> None:
 
     for i, cls in enumerate(clses):
         duration = timeit.timeit(
-            "a.some_func()",
-            setup="from __main__ import {0}; a = {0}('X.Y')".format(cls),
-            number=number)
+            "a.some_func()", setup="from __main__ import {0}; a = {0}('X.Y')".format(cls), number=number)
         durations[i] = duration
 
     writeln_utf8("Benchmarking invariant at a function:\n")
